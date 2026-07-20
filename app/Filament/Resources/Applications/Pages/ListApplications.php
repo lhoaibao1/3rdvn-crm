b0bb6c0b27493ead20c544acd0a9de10a67cde61
lhoaibao1 <130900967+lhoaibao1@users.noreply.github.com>
2026-07-21 03:14:33 +0700

    Move application create action into table

--- app/Filament/Resources/Applications/Pages/ListApplications.php
+++ app/Filament/Resources/Applications/Pages/ListApplications.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Applications\Pages;
 
 use App\Filament\Resources\Applications\ApplicationResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListApplications extends ListRecords
@@ -15,10 +14,4 @@ class ListApplications extends ListRecords
         return '';
     }
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make()->label('Tạo hồ sơ'),
-        ];
-    }
 }
