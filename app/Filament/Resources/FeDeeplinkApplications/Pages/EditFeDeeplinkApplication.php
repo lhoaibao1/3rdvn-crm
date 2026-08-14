886327e857c7dd2f1986bed5c25b031956172554
Mac <mac@Macs-MacBook-Air.local>
2026-08-14 13:00:27 +0700

    feat: expose FE Deeplink in CRM navigation

---
+++ app/Filament/Resources/FeDeeplinkApplications/Pages/EditFeDeeplinkApplication.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FeDeeplinkApplications\Pages;
+
+use App\Filament\Resources\Applications\Pages\EditApplication;
+use App\Filament\Resources\FeDeeplinkApplications\FeDeeplinkApplicationResource;
+
+class EditFeDeeplinkApplication extends EditApplication
+{
+    protected static string $resource = FeDeeplinkApplicationResource::class;
+}
