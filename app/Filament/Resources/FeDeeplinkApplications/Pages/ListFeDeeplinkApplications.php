b6dda5b49da6f8bf484dfedcba292f8c678e2610
3RDVN Development <dev@3rd-vn.io.vn>
2026-08-15 11:36:19 +0700

    fix: restore FE Deeplink list heading

--- app/Filament/Resources/FeDeeplinkApplications/Pages/ListFeDeeplinkApplications.php
+++ app/Filament/Resources/FeDeeplinkApplications/Pages/ListFeDeeplinkApplications.php
@@ -8,4 +8,14 @@ use App\Filament\Resources\FeDeeplinkApplications\FeDeeplinkApplicationResource;
 class ListFeDeeplinkApplications extends ListApplications
 {
     protected static string $resource = FeDeeplinkApplicationResource::class;
+
+    public function getHeading(): string
+    {
+        return 'Danh sách khách hàng FE Deeplink';
+    }
+
+    public function getSubheading(): ?string
+    {
+        return 'FE - Cash Loan - Deeplink';
+    }
 }
